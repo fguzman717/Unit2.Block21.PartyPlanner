09c78fd63b1f0154100b5b51cd96755004ce69fc
fguzman717 <felixguzman717@gmail.com>
2024-12-04 21:36:17 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const cohortID = "2406-FTB-MT-WEB-PT";
+const webURL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${cohortID}`;
+
+//Fetch is used correctly to GET party data from the API.
+
+//Fetch is used correctly to POST a new party to the API.
+
+//Fetch is used correctly to DELETE a party from the API.
