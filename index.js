c9a12206e0eda0749d6354937381f26e4a585ab1
fguzman717 <felixguzman717@gmail.com>
2024-12-05 13:29:01 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,8 +1,93 @@
 const cohortID = "2406-FTB-MT-WEB-PT";
 const webURL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${cohortID}`;
 
-//Fetch is used correctly to GET party data from the API.
+const state = {
+  parties: [],
+};
 
-//Fetch is used correctly to POST a new party to the API.
+const partyList = document.querySelector("#parties");
 
-//Fetch is used correctly to DELETE a party from the API.
+const addPartyForm = document.querySelector("#addParty");
+
+addPartyForm.addEventListener("submit", addNewParty);
+
+//GET party data from the API.
+async function getPartyData() {
+  try {
+    const response = await fetch(`${webURL}/events`);
+    const json = await response.json();
+    state.parties = json.data;
+    renderPartyData();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Render list of the names, dates, times, locations, and descriptions of all parties
+function renderPartyData() {
+  if (!state.parties.length) {
+    partyList.innerHTML = "<li>There are no parties.</li>";
+    return;
+  }
+
+  const partyCards = state.parties.map((party) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+      <h2>${party.name}</h2>
+      <p>${party.date}</p>
+      <p>${party.location}</p>
+      <p>${party.description}</p>
+      <button onClick="deleteParty(${party.id})">Delete</button>
+    `;
+    return li;
+  });
+
+  partyList.replaceChildren(...partyCards);
+}
+
+// POST a new party to the API.
+async function addNewParty(event) {
+  event.preventDefault();
+  try {
+    const response = await fetch(`${webURL}/events`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: addPartyForm.name.value,
+        date: addPartyForm.date.value,
+        location: addPartyForm.location.value,
+        description: addPartyForm.description.value,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to add party!");
+    }
+
+    getPartyData();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+//DELETE a party from the API.
+async function deleteParty(id) {
+  try {
+    const response = await fetch(`${webURL}/events/${id}`, {
+      method: "DELETE",
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to delete party!");
+    }
+
+    console.log("Party was successfully deleted.");
+    getPartyData();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getPartyData();
